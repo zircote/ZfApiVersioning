0a5113f49bd5096c42d0e3f06045968370b802eb
Robert Allen <zircote@gmail.com>
2012-02-06 21:30:52 -0600

    added zf mvc examples

---
+++ application/modules/v2/Bootstrap.php
@@ -0,0 +1,5 @@
+<?php
+class V2_Bootstrap extends Zend_Application_Module_Bootstrap
+{
+
+}
---
+++ application/plugins/Version.php
@@ -0,0 +1,19 @@
+<?php
+
+class Application_Plugin_Version extends Zend_Controller_Plugin_Abstract
+{
+    public function preDispatch($request)
+    {
+        /* @var $bootstrap Zend_Application_Bootstrap_Bootstrap */
+        if($request->getModuleName() == 'api'){
+            $bootstrap = Zend_Controller_Front::getInstance()->getParam('bootstrap');
+            $defaultVersion = $bootstrap->getOption('defaultVersion');
+            $apiVersion = (string) $request->getParam('version', $defaultVersion);
+            $request->setModuleName($apiVersion)
+                ->setControllerName($request->getControllerName())
+                ->setActionName($request->getActionName())
+                ->setDispatched(false);
+//             print_r($request); exit;
+        }
+    }
+}
